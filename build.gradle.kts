264fdb009b8b81997335ae77694104cec7d092bc
Maxim Kulikov <info@maximkulikov.ru>
2021-04-09 22:31:38 +0300

    обновлен ридми

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,33 +3,40 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
-    id("org.jetbrains.compose") version "0.2.0-build132"
+   kotlin("jvm") version "1.4.20"
+   id("org.jetbrains.compose") version "0.2.0-build132"
 }
 
 group = "ru.gavarent"
-version = "1.0"
+version = "1.3"
 
 repositories {
-    jcenter()
-    mavenCentral()
-    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+   jcenter()
+   mavenCentral()
+   maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
 }
 
 dependencies {
-    implementation(compose.desktop.currentOs)
+   implementation(compose.desktop.currentOs)
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+   kotlinOptions.jvmTarget = "11"
 }
 
 compose.desktop {
-    application {
-        mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "email-validation"
-        }
-    }
+   application {
+      mainClass = "MainKt"
+      nativeDistributions {
+         version = "1.3"
+         targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+         packageName = "email-validation"
+         this.windows {
+            dirChooser = true
+            shortcut = true
+            menuGroup = "Email"
+            menu = true
+         }
+      }
+   }
 }
